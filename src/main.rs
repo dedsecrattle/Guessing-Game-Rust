23fb9f93fb30ec122c8044af82e00dae11379af3
Prabhat Kumar <itsprabxxx@gmail.com>
2024-04-29 17:24:49 +0800

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use std::{cmp::Ordering, io::stdin};
+use rand::{thread_rng, Rng};
+use colored::Colorize;
+
+
+fn main() {
+    println!("Welcome to the Game! \nType quit to exit the Game \n");
+
+    let mut score = 0;
+
+    let secretnum = thread_rng().gen_range(1..=100);
+    loop {
+         
+        println!("Please Guess a number Between 1 to 100 !");
+        let mut guess = String::new();
+
+        stdin().read_line(&mut guess).expect("ERROR: While Taking Input!");
+
+        if guess.trim() == "quit" {
+            break;
+        }
+
+        let guess:u32 = match guess.trim().parse() {
+            Ok(val) => {
+                if (val <= 100) & (val >= 1) {
+                    val
+                } else {
+                    continue;
+                }
+            },
+            Err(_) => {
+                continue;
+            },
+        };
+
+        match guess.cmp(&secretnum) {
+            Ordering::Less => println!("{}","Your Guess is too Less!".red()),
+            Ordering::Equal => {
+                println!("{} {}", "You Won , Your Score is".green(),score);
+                break;
+            },
+            Ordering::Greater=> println!("{}","Your Guess is tooo High!".red())
+        }
+        println!("\n=================================\n");
+        score = score + 1;
+    }
+}
